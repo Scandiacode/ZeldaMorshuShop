2a646b1740b919828360028fd00d323d2c1efc33
Logan Harris <logan.harris41@yahoo.com>
2021-10-05 20:40:27 -0400

    finish lab

--- Program.cs
+++ Program.cs
@@ -8,67 +8,92 @@ namespace ShoppingListLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Lamp Oil, Rope, Bombs, you want it. Its your my friend, as long as you have enough Rupees.");
+            Console.WriteLine("Lamp Oil, Rope, Bombs, you want it. Its your my friend, as long as you have enough Rupees.\n");
 
-            while (true)
+            Dictionary<string, double> morshuInventory = new Dictionary<string, double>();
+            List<string> itemNames = new List<string>();
+            List<double> itemPrices = new List<double>();
+            morshuInventory.Add("Lamp Oil", 5);
+            morshuInventory.Add("Rope", 10);
+            morshuInventory.Add("Bomb", 20);
+            morshuInventory.Add("Magical Shield", 160);
+            morshuInventory.Add("Small Key", 100);
+            morshuInventory.Add("Blue Candle", 60);
+            morshuInventory.Add("Arrows", 80);
+            morshuInventory.Add("Food", 100);
+            morshuInventory.Add("Recovery Heart", 10);
+            morshuInventory.Add("Blue Ring", 250);
+
+            string runProgram = "y";
+            do
             {
-                Console.WriteLine("What Item do you want to buy, Link?");
-                string itemChoice = Console.ReadLine();
-                /*
-                if ()
+
+                foreach (KeyValuePair<string, double> item in morshuInventory)
                 {
-                    Console.WriteLine("Hey Link");
+                    Console.WriteLine($"{item.Key}  {item.Value}");
+                }
+
+                Console.WriteLine("");
+
+                Console.WriteLine("What Item do you want to buy, Link?");
+                string userInput = Console.ReadLine();
+                if (!morshuInventory.ContainsKey(userInput))
+                {
+                    Console.WriteLine("Sorry Link, I don't sell that.");
+                    continue;
                 }
                 else
                 {
-                    Console.WriteLine("Sorry Link, I don't sell that.");
-                    break;
+                    itemNames.Add(userInput);
+                    itemPrices.Add(morshuInventory.GetValueOrDefault(userInput, 0));
+                    Console.WriteLine($"added {userInput} for {morshuInventory.GetValueOrDefault(userInput, 0)} Rupees");
                 }
-                */
-            }
 
-            
-            Console.WriteLine("Sorry Link, I can't give credit. Come back when your little mmmmmm...richer.");
+                Console.WriteLine("");
 
-           
-        }
+                do
+                {
+                    Console.WriteLine("Any other item you would like to buy, Link? (y/n)");
+                    runProgram = Console.ReadLine().ToLower();
+                    if (runProgram.Equals("y"))
+                    {
+                        break;
+                    }
+                    else if (runProgram.Equals("n"))
+                    {
+                        break;
+                    }
+                        
+                } while (true);
 
-        static void MenuDisplayTable(int itemChoice)
-        {
+            } while (runProgram == "y");
 
 
-            List<string> itemNames = new List<string> {
-
-                    "Lamp Oil",
-                    "Rope",
-                    "Bomb",
-                    "Magical Shield",
-                    "Small Key",
-                    "Blue Candle",
-                    "Arrows",
-                    "Food",
-                    "Recovery Heart",
-                    "Blue Ring"
-                };
-
-            List<int> itemPrices = new List<int> {
-
-                    5,
-                    10,
-                    20,
-                    160,
-                    100,
-                    60,
-                    80,
-                    100,
-                    10,
-                    250
-                };
 
             Console.WriteLine(string.Format("{0,15} {1,15}", "Item", "Price"));
             Console.WriteLine(string.Format("{0,15} {1,15}", "=======", "======="));
+            for (int i = 0; i < itemNames.Count; i++)
+            {
+                Console.WriteLine(string.Format("{0,15} {1,15}", itemNames[i], itemPrices[i]));
+            }
+            Console.WriteLine("========================================");
+            Console.WriteLine($"Link that averages out to {itemPrices.Average()} Rupees\n");
 
+            Console.WriteLine("Will you pay Link? (y/AHH)");
+            string morshuReject = Console.ReadLine();
 
+            if (morshuReject.Equals("y"))
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Rupee Balance: 0 \n");
+                Console.WriteLine("Sorry Link, I can't give credit. Come back when your little mmmmmm...richer.");
+            }
+            else
+            {
+                Console.WriteLine("*Link picks up his sword to attack Morshu* AHHHHHhhhHhHhhHhHHHHHh");
+            }
+
+            
 
         }
     }
